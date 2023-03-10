7f8b0a492b298b3a05deabdc5aa49fcbad1e6d95
chengniu <chengniu@gmail.com>
2023-03-10 15:29:43 +0800

    Update TrainTruecaser.py

--- TrainTruecaser.py
+++ TrainTruecaser.py
@@ -14,6 +14,7 @@ nltk.download('reuters')
 nltk.download('semcor')
 nltk.download('conll2000')
 nltk.download('state_union')
+nltk.download('punkt')
 
 uniDist = nltk.FreqDist()
 backwardBiDist = nltk.FreqDist() 
